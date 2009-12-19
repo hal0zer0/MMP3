417f1dcea5afcea02a0475b9aa2753ab98e24aac
Josh Price <price.joshuad@gmail.com>
2009-12-19 00:16:12 -0800

    added proper track info reading for MP3 files

--- playlist.py
+++ playlist.py
@@ -17,15 +17,40 @@ class Playlist(gtk.ListStore):
     filenameToAdd = add_file_dialog.show()
     ext = filenameToAdd[-3:].lower()
     print "Extension is", ext
-    if (filenameToAdd != None) and  (ext == "mp3"):
-      trackInfo = self._get_mp3_trackInfo(filenameToAdd)
-      print "Appending", trackInfo
-      self.append(trackInfo)
-      #self.append(["99", "Artist", "Song",  "0:00", filenameToAdd])
+    if filenameToAdd != None:
+      if ext == "mp3":
+        trackInfo = self._get_mp3_trackInfo(filenameToAdd)
+        self.append(trackInfo)
+    self.update_track_numbers()
+
 
   def _get_mp3_trackInfo(self, filename):
-    return ["99", "Boogie Down", "Yourmomtones", "4:20", "/home"]
+    #Get artist and title from ID3
+    try:
+      id3Info = EasyID3(filename)
+      artist = id3Info["artist"][0].strip()
+      title = id3Info["title"][0].strip()
+    except Exception as err:
+      print err
+      artist = "Unknown"
+      title = "Unknown"
 
+    #Get length info from MP3 file
+    try:
+      file_info = MP3(filename)
+      secondslong = int(file_info.info.length)
+      length = str(datetime.timedelta(seconds=secondslong))
+    except:
+      length = "0:00"
+
+    return [len(self) + 1, title, artist, length, filename]
+
+
+  def update_track_numbers(self):
+    i = 1
+    for line in self:
+      line[0] = str(i).zfill(2)
+      i += 1
 
 
 
